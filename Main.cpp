0c8a2d12030c32d5942b7643de344db5379abe9a
Cong <Cong@Mac.local>
2013-12-08 00:10:16 -0600

    + workaround for Mavericks

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,24 @@
 #include "MainWindow.h"
 #include <QApplication>
+#include <QDir>
+
+// workaround for a bug on Mavericks
+// Finder returns / as the working path of an app bundle
+// but if the app is run from terminal, the path is correct
+// This method calcluates the path of the bundle from the application's path
+QString getCurrentPath()
+{
+    QDir dir(QApplication::applicationDirPath());
+    dir.cdUp();
+    dir.cdUp();
+    dir.cdUp();
+    return dir.absolutePath();
+}
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QDir::setCurrent(getCurrentPath());
     MainWindow wnd;
     wnd.showMaximized();
     return app.exec();
